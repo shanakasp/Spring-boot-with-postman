0bf38d67983bbc78b82eac977741724bdbef1ed2
shanaka prince <shanakaprince@gmail.com>
2023-01-25 11:49:47 +0530

    First commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+public class UserController {
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "Simple-Root";
+    }
+    @PostMapping("/saveUser")
+    public String saveUser(){
+        return "User Saved! " ; }
+
+    @PutMapping("/updateUser")
+    public String updateUser(){
+        return "User Updated!" ;}
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(){
+        return "User Deleted! " ;}
+
+}
